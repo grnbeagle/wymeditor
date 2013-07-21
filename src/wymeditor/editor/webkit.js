4e2e19ee8fd56b83fc97b199f83aaaf33466811c
Amie Kweon <amiekweon@gmail.com>
2013-07-21 16:15:10 -0700

    Remove mysterious span tags added when inserting linebreak

--- src/wymeditor/editor/webkit.js
+++ src/wymeditor/editor/webkit.js
@@ -110,8 +110,7 @@ WYMeditor.WymClassSafari.prototype._exec = function (cmd, param) {
         // If the container is a span, strip it out if it doesn't have a class
         // but has an inline style of 'font-weight: normal;'.
         if (tagName === 'span' &&
-            !$container.attr('class') &&
-            $container.attr('style') === 'font-weight: normal;') {
+            !$container.attr('class')) {
 
             $container.contents().unwrap();
         }
